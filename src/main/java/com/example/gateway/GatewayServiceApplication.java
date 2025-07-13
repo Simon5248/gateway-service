b2421ab0407333faaed8bcff1dd1a5e0b8bd0bb3
Simon9791 <Simon9791@Gmail.com>
2025-07-13 08:31:08 +0800

    啟用 Eureka 客戶端和 Feign 客戶端，移除資料源自動配置排除設定

--- src/main/java/com/example/gateway/GatewayServiceApplication.java
+++ src/main/java/com/example/gateway/GatewayServiceApplication.java
@@ -2,9 +2,12 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
